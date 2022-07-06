60cce788c61ab3660f0c836b16cbb782f5163590
Erica Owusua Boahemaa <mcrenoldj@email.com>
2022-07-05 22:50:13 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - return abs value
+ * @number : number to check
+ * Return:0 or 1
+ */
+
+int _abs(int number)
+{
+
+return (number >= 0 ? number : number * -1);
+
+}
