2ddfc2ba78a02dfbd7b66fdc1601a60bfd1a500c
Erica Owusua Boahemaa <mcrenoldj@email.com>
2022-07-05 22:02:10 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * This will Return:0
+ */
+
+int main(void)
+{
+char c[9] = "_putchar";
+int itrate = 0;
+while (8 > itrate)
+{
+_putchar(c[itrate]);
+itrate++;
+}
+_putchar('\n');
+return (0);
+}
