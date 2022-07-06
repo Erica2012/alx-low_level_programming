e22bc93c9e2aa46b7e58fbc5c0877a6897007cde
Erica Owusua Boahemaa <mcrenoldj@email.com>
2022-07-05 22:06:45 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Entry point
- * This will Return:0
+ * Return:0
  */
 
 int main(void)
 {
 char c[9] = "_putchar";
 int itrate = 0;
-while (8 > itrate)
+while (itrate < 8)
 {
 _putchar(c[itrate]);
 itrate++;
