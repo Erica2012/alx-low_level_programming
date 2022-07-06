354fdf587fba83dec9bb82f5963104e1fe7fad92
Erica Owusua Boahemaa <mcrenoldj@email.com>
2022-07-05 22:13:58 -0700

    taskt 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int iterate = 0;
+
+	while (iterate < 10)
+	{
+		alphabet = 'a';
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+		iterate++;
+	}
+
+}
+
